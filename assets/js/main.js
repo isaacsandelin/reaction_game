134762a02b9303c52f4e9f86513baaa41c43addf
isaacsandelin <isaaceltonsandelin@gmail.com>
2022-12-07 09:56:09 +0000

    Created function play

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,7 @@ const MAXIMUM_MS_TILL_CHANGE = 10000;
 
 let msSinceEpochOnTimeout = 0;
 let waitingForClick = false;
+
+function play() {
+    const msTillChange = Math.floor(Math.random() * (MAXIMUM_MS_TILL_CHANGE - MINIMUM_MS_TILL_CHANGE)) + MINIMUM_MS_TILL_CHANGE;
+}
