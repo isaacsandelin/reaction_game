86c18d4b95418ca97b424732609ea679570e4f4f
isaacsandelin <isaaceltonsandelin@gmail.com>
2022-12-07 09:52:40 +0000

    Changed the minimum and maximum time that decides when the game should start

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ const displayText = document.querySelector(".display-text");
 const scoreElements = document.querySelectorAll(".score");
 
 const scoreHistory = [];
+
+const MINIMUM_MS_TILL_CHANGE = 3000;
+const MAXIMUM_MS_TILL_CHANGE = 10000;
+
+let msSinceEpochOnTimeout = 0;
+let waitingForClick = false;
